9682f2e39b9fe73139cd8bd7985b7d519b693a44
Sandeep Mehta <sandeep.mehta@bitontree.com>
2022-07-12 01:34:15 +0530

    added files required

--- src/containers/Contact/index.tsx
+++ src/containers/Contact/index.tsx
@@ -4,20 +4,22 @@ import BetCard from 'components/ContactCard';
 import HandicapperCard from 'components/ContactTable';
 const cardData = [
   {
-    cardTitle: 'test 2 x $10 Free Bets - No Deposit Required',
-    termsandconditions: "T&C's apply. NJ, PA, WV, CO, IN, IL, VA, IA, TN residents only. 21+.",
-    isRecommendedOffer: true,
-    rating: 5,
-    siteImage: '46491fab-9df6-4533-8fa2-9f3e78dfb513.png',
+    fullName: 'Deposit Required',
+    title: 'Product manager',
+    email: "T&C's apply. NJ, PA, WV, CO, IN, IL, VA, IA, TN residents only. 21+.",
+    phone: true,
+    company: 5,
+    address: '46491fab-9df6-4533-8fa2-9f3e78dfb513.png',
     id: '6082f82dadf18c211821b544',
   },
   {
-    cardTitle: 'test Risk Free Bet Up To $1000',
-    termsandconditions: "T&C's apply. NJ, PA, WV, CO, IN, IL, VA, IA, TN residents only. 21+.",
-    isRecommendedOffer: false,
-    rating: 2,
-    siteImage: '663ecbe4-a73e-4eee-a12f-9de612dc986c.png',
-    id: '60ab34a7ac2f862f68ff4762',
+    fullName: 'Deposit Required',
+    Email: "T&C's apply. NJ, PA, WV, CO, IN, IL, VA, IA, TN residents only. 21+.",
+    title: 'Product manager',
+    phone: true,
+    company: 5,
+    address: '46491fab-9df6-4533-8fa2-9f3e78dfb513.png',
+    id: '6082f82dadf18c211821b544',
   },
 ];
 
